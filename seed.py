0583bccce521a97e8b719f719a5709a1206f97ce
hedgyv <hedgy85@hotmail.com>
2023-12-10 18:33:30 +0200

    WEB_DZ7

---
+++ seed.py
@@ -0,0 +1,47 @@
+from random import randint, choice
+from faker import Faker
+from sqlalchemy.exc import SQLAlchemyError
+from conf.db import session
+from conf.models import Teacher, Student, Group, Subject, Grade
+
+fake = Faker()
+
+amount_of_students = randint(30,50)
+amount_of_groups = 3
+amount_of_subjects = randint(5,8)
+amount_of_teachers = randint(3,5)
+amount_students_in_each_group = int(amount_of_students/amount_of_groups)
+
+def insert_groups():
+    for _ in range(amount_of_groups):
+        group = Group(
+            name=fake.word()
+        )
+        session.add(group)
+
+def insert_students():
+    for _ in range(amount_of_students):
+        student = Student(
+            fullname=fake.name(),
+            group_id=choice(session.query(Group.id).all())[0],   
+        )
+        session.add(student)
+        
+if __name__ == '__main__':
+    if __name__ == '__main__':
+        try:
+            insert_groups()
+            insert_students()
+            #insert_teachers()
+            #insert_subjects()
+            session.commit()
+            
+            students = session.query(Student).all()
+            subjects = session.query(Subject).all()
+            print(subjects)
+            session.commit()
+        except SQLAlchemyError as e:
+            print(e)
+            session.rollback()
+        finally:
+            session.close()
