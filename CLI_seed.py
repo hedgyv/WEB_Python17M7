c00f1785b48a798c6199055f346297574e070d2f
hedgyv <hedgy85@hotmail.com>
2023-12-11 17:45:56 +0200

    WEB_DZ7

--- CLI_seed.py
+++ CLI_seed.py
@@ -6,13 +6,44 @@ from conf.db import session
 import configparser
 import pathlib
 
-def create(args):
-    
+def create_teacher(args):
+
     teacher = Teacher(fullname=args.fullname)
     session.add(teacher)
     session.commit()
+
+    print(f'A teacher has been created {args.fullname}')
     
-    print(f'Создан учитель с именем {args.fullname}')
+def create_group(args):
+
+    group = Group(name=args.name)
+    session.add(group)
+    session.commit()
+
+    print(f'A group has been created {args.name}')
+    
+def create_subject(subject_name, teacher_id):
+
+    group = Subject(name=subject_name, teacher_id=teacher_id)
+    session.add(group)
+    session.commit()
+
+    print(f'A subject has been created {subject_name, teacher_id}')
+
+def create_student(student_name, group_id):
+
+    student = Student(fullname=student_name, group_id=group_id)
+    session.add(student)
+    session.commit()
+
+    print(f'A student has been created {student_name, group_id}')
+    
+def create_grade(grade, grade_date, student_id, subject_id):
+    st_grade = Grade(grade=grade, grade_date=grade_date, student_id=student_id, subject_id=subject_id)
+    session.add(st_grade)
+    session.commit()
+
+    print(f'A grade has been created {grade, grade_date, student_id, subject_id}')
 
 def list(args):
     ...
@@ -33,11 +64,16 @@ def main():
     # subparsers.required = True
 
     # create_parser = subparsers.add_parser('create', help='Создать учителя')
-    parser.add_argument('--action', '-a', choices=['create', 'list', 'update', 'remove'], help='CRUD операции')
-    parser.add_argument('--model', '-m', choices=['Teacher'], help='Укажите модель')
-    parser.add_argument('--fullname','-n', required=True, help='Имя учителя')
-    parser.set_defaults(func=create)
-
+    parser.add_argument('--action', '-a', choices=['create', 'list', 'update', 'remove'], help='CRUD operations')
+    parser.add_argument('--model', '-m', choices=['Student','Teacher', 'Group', 'Subject', 'Grade'], help='Specify the model')
+    parser.add_argument('--stfullname', '-stn', nargs=2, required=False, help='Student name')
+    parser.add_argument('--fullname', '-n', required=False, help='Teacher name')
+    parser.add_argument('--name','-gn', required=False, help='Group name')
+    parser.add_argument('--sname','-sn', nargs=2, required=False, help='Subject name')
+    parser.add_argument('--graname','-grn', nargs=4, required=False, help='Grade name')
+    
+    
+    #__________________Will be created later_________________________________________
     # list_parser = subparsers.add_parser('list', help='Вывести всех учителей')
     # list_parser.set_defaults(func=list)
 
@@ -51,7 +87,37 @@ def main():
     # remove_parser.set_defaults(func=remove)
 
     args = parser.parse_args()
-    args.func(args)
+    
+    if args.action == 'create':
+        if args.model == 'Teacher':
+            if args.fullname:
+                create_teacher(args)
+            else:
+                print("To create a teacher the name should be specified.")
+        elif args.model == 'Group':
+            if args.name:
+                create_group(args)
+            else:
+                print("To create a group the name should be specified.")
+        elif args.model == 'Subject':
+            if args.sname:
+                subject_name, teacher_id = args.sname
+                create_subject(subject_name, teacher_id)
+            else:
+                print("To create a subject the name should be specified.")
+        elif args.model == 'Student':
+            if args.stfullname:
+                student_name, group_id = args.stfullname
+                create_student(student_name, group_id)
+            else:
+                print("To create a student the name should be specified.")
+        elif args.model == 'Grade':
+            if args.graname:
+                grade, grade_date, student_id, subject_id = args.graname
+                create_grade(grade, grade_date, student_id, subject_id)
+            else:
+                print("To create a student the name should be specified.")
+        
 
 if __name__ == '__main__':
     main()
